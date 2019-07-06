7dbb01a6b6079f5450ebef4ff5a403de3cf89f86
Philipp Kopper <philipp.kopper@am-ammersee.de>
2019-07-06 12:47:01 +0200

    added new file for packages [ci skip]

--- R-scripts/new_real.R
+++ R-scripts/new_real.R
@@ -1,23 +1,9 @@
-remove.packages("lime")
-library(mlr)
-library(devtools)
-install_github("https://github.com/pkopper/lime/tree/gower")
-library(dplyr)
-library(lime)
-explain <- lime::explain
-library(MASS)
-library(ggplot2)
-library(reshape2)
-
-library(readr)
 day <- read_csv("datasets/day.csv")
 day$holiday <- as.factor(day$holiday)
 day$workingday <- as.factor(day$workingday)
 day <- day[, c(3, 6, 8:10, 12:13, 16)]
 day$season <- as.factor(day$season)
 day$weathersit <- as.factor(day$weathersit)
-library(data.table)
-library(mltools)
 day <- as.data.frame(day)
 data_set <- make_split(day, 0.85)
 
@@ -30,8 +16,7 @@ learner <- makeLearner("regr.randomForest", ntree = 50)
 black_box <- train(learner, task)
 ### predict
 task_pred <- predict(black_box, newdata = data_set$test)
-ranking <- rank_predictions(task_pred)
-sum(ranking)
+mean(abs(task_pred$data$truth - task_pred$data$response))
 
 # fit not as good as before but not that important because we don't know truth.
 ggplot(data = task_pred$data, aes(x = response, y = truth)) +
@@ -94,8 +79,7 @@ learner <- makeLearner("regr.earth")
 black_box <- train(learner, task)
 ### predict
 task_pred <- predict(black_box, newdata = data_set$test)
-ranking <- rank_predictions(task_pred)
-sum(ranking)
+mean(abs(task_pred$data$truth - task_pred$data$response))
 
 # fit not as good as before but not that important because we don't know truth.
 ggplot(data = task_pred$data, aes(x = response, y = truth)) +
@@ -109,20 +93,37 @@ explainer <- lime(data_set$train[, 1:8], black_box, bin_continuous = FALSE,
 kernel_widths <- c(seq(0.07, 0.15, 0.02), seq(0.275, 1, 0.075),
                    seq(1.15, 1.75, 0.15), seq(1.75, 5.75, 1), 20)
 
-km_real2 <- vector(mode = "list", length = 8)
-
+km_real2 <- vector(mode = "list", length = 5)
+km_real2_abs <- vector(mode = "list", length = 5)
 for (i in 1:length(km_real2)) {
   km_real2[[i]] <- try(analyse_multivariate_kernel_width(kernel_widths,
-                                                         data_set$test[i, 1:8], 
+                                                         data_set$test[i, 1:7], 
                                                          explainer,
-                                                         n_features = 4, 
-                                                         n_permutations = 8000, 
+                                                         n_features = 3, 
+                                                         n_permutations = 6500, 
                                                          dist_fun = "euclidean",
                                                          seed = 1,
                                                          ci = TRUE,
                                                          feature_select = 
                                                            "auto",
                                                          iterations = 48))
+}
+
+for (i in 1:length(km_real2_abs)) {
+  km_real2_abs[[i]] <- try(analyse_multivariate_kernel_width(kernel_widths,
+                                                            data_set$test
+                                                            [i, 1:7], 
+                                                            explainer,
+                                                            n_features = 3, 
+                                                            n_permutations = 
+                                                              6500, 
+                                                            dist_fun = 
+                                                              "euclidean",
+                                                            seed = 1,
+                                                            ci = TRUE,
+                                                            feature_select = 
+                                                              "highest_weights",
+                                                            iterations = 48))
 }
 
 panels <- vector(mode = "list", length = 3)
@@ -138,3 +139,5 @@ png("04-09-15.png", width = 2600, height = 800)
 grid.arrange(panels[[1]], panels[[2]], panels[[3]], nrow = 1)
 dev.off()
 
+saveRDS(km_real, file = "R-results/kernelmatrix-bike-randomforest")
+saveRDS(km_real2, file = "R-results/kernelmatrix-bike-earth")
---
+++ R-scripts/packages.R
@@ -0,0 +1,33 @@
+required_packages <- c("devtools", 
+                       "mlr", 
+                       "ggplot2",
+                       "reshape2",
+                       "gridExtra",
+                       "dplyr",
+                       "readr",
+                       "randomForest",
+                       "earth", 
+                       "MASS")
+install_these <- 
+  required_packages[!(required_packages %in% installed.packages())]
+install.packages(install_these)
+if ("lime" %in% installed.packages()) {
+  warning(paste("You already have the lime installed.", 
+                "Make sure to use a current github version."))
+} else {
+  library(devtools)
+  install_github("https://github.com/thomasp85/lime")
+}
+
+library(mlr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(dplyr)
+library(readr)
+library(randomForest)
+library(earth)
+library(MASS)
+library(lime)
+
+explain <- lime::explain
